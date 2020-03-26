61c1d8769d67642fe89ed67f727c8d9c6629fe60
Scott Seago <sseago@redhat.com>
2020-03-26 13:09:47 -0400

    Added support for restic --verify flag
    Added support for restic volume checksum verification.
    This adds another pod annotation ('backup.velero.io/verify-volumes')
    which is used to identify which volumes on the pod should get the
    '--verify' flag passed to restic when restoring.
    
    Stdout and stderr are logged to the restic pod logs. The PodVolumeRestore
    CRD has three new status fields: Errors, VerifyErrors, and ResticPod,
    which contain the number of total errors reported, the number of verify
    errors reported, and the name of the restic pod that ran the restore
    (to allow for the user to look in the pod logs for more detailed output).
    
    The Restore CRD has two new Status fields: PodVolumeRestoreErrors
    and PodVolumeRestoreVerifyErrors, which contain a slice of ObjectReferences
    listing which PodVolumeRestores contained errors or verify errors.
    
    There is not yet support built into restore describe to see the restore fields
    via the velero client. Also, the restic daemonset will need to have an env
    variable set for POD_NAME.
    
    (cherry picked from commit da2df66ad156ccd3c86dfb885634d744abc1b980)
    (cherry picked from commit 5a05c6be5711fdcc93cbba7c53fdb3ece531cdde)
    Signed-off-by: Scott Seago <sseago@redhat.com>
    
    Refactored restic verify to use output annotations rather than (#146)
    
    adding new status fields. This removes the CRD changes from upstream.
    
    making sure pvr errors are getting reported correctly

--- pkg/podvolume/util.go
+++ pkg/podvolume/util.go
@@ -42,6 +42,38 @@ const (
 	// VolumesToExcludeAnnotation is the annotation on a pod whose mounted volumes
 	// should be excluded from restic backup.
 	VolumesToExcludeAnnotation = "backup.velero.io/backup-volumes-excludes"
+
+	// VolumesToVerifyAnnotation is the annotation on a pod whose mounted volumes
+	// need to be verified after backing up with restic.
+	VolumesToVerifyAnnotation = "backup.velero.io/verify-volumes"
+
+	// PVRErrorsAnnotation is the annotation on a PVR indicating the number of
+	// errors on running restic restore.
+	PVRErrorsAnnotation = "velero.io/pvr-errors"
+
+	// PVRVerifyErrorsAnnotation is the annotation on a PVR indicating the number of
+	// verify errors on running restic restore.
+	PVRVerifyErrorsAnnotation = "velero.io/pvr-verify-errors"
+
+	// PVRPodNameAnnotation is the annotation on a PVR indicating the name of the
+	// restic pod that ran the restic restore (for looking up error messages)
+	PVRResticPodAnnotation = "velero.io/pvr-restic-pod"
+
+	// RestorePVRErrorsAnnotation is the annotation on a restore providing a list of
+	// PVRs with errors on running restic restore.
+	RestorePVRErrorsAnnotation = "velero.io/restore-pvr-errors"
+
+	// RestorePVRVerifyErrorsAnnotation is the annotation on a restore providing a list of
+	// PVRs with verify errors on running restic restore.
+	RestorePVRVerifyErrorsAnnotation = "velero.io/restore-pvr-verify-errors"
+
+	// RestorePVRErrorCountAnnotation is the annotation on a restore specifying how many
+	// PVRs had errors on running restic restore.
+	RestorePVRErrorCountAnnotation = "velero.io/restore-pvr-error-count"
+
+	// RestorePVRVerifyErrorCountAnnotation is the annotation on a restore specifying how many
+	// PVRs had verify errors on running restic restore.
+	RestorePVRVerifyErrorCountAnnotation = "velero.io/restore-pvr-verify-error-count"
 )
 
 // GetVolumeBackupsForPod returns a map, of volume name -> snapshot id,
@@ -148,6 +180,27 @@ func getVolumesToExclude(obj metav1.Object) []string {
 func contains(list []string, k string) bool {
 	for _, i := range list {
 		if i == k {
+			return true
+		}
+	}
+	return false
+}
+
+// GetVolumesToVerifyIncludes returns true if the passed-in
+// volume name is included in the VolumesToVerifyAnnotation
+func GetVolumesToVerifyIncludes(obj metav1.Object, volName string) bool {
+	annotations := obj.GetAnnotations()
+	if annotations == nil {
+		return false
+	}
+
+	volumesValue := annotations[VolumesToVerifyAnnotation]
+	if volumesValue == "" {
+		return false
+	}
+
+	for _, vol := range strings.Split(volumesValue, ",") {
+		if vol == volName {
 			return true
 		}
 	}
