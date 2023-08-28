bb239e47d1f1ea99b303117f8240e0ff54e58ddc
Tiger Kaovilai <tkaovila@redhat.com>
2023-08-28 10:09:37 -0400

    Handle 1.27 k8s job label changes
    per  https://github.com/kubernetes/kubernetes/blob/0e86fa5115cb79a04bdb949d135e35a31ce806f2/CHANGELOG/CHANGELOG-1.27.md?plain=1#L1768
    
    Signed-off-by: Tiger Kaovilai <tkaovila@redhat.com>

--- pkg/discovery/helper.go
+++ pkg/discovery/helper.go
@@ -285,3 +285,11 @@ func (h *helper) ServerVersion() *version.Info {
 	defer h.lock.RUnlock()
 	return h.serverVersion
 }
+
+func ServerVersion(logger logrus.FieldLogger) (*version.Info, error) {
+	discoveryHelper, err := NewHelper(&discovery.DiscoveryClient{}, logger)
+	if err != nil {
+		return nil, err
+	}
+	return discoveryHelper.ServerVersion(), nil
+}
